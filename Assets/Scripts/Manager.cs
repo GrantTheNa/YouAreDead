baa09fdc82ded695fc5cc204878f564271feff4d
GrantTheNa <82065825+GrantTheNa@users.noreply.github.com>
2022-07-19 10:46:36 +1000

    Respawn Setup

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -11,12 +11,22 @@ public class Manager : MonoBehaviour
     public GameObject F_SecretDoor;
     public GameObject F_SecretDoor2;
     public GameObject F_Fire;
+    public GameObject F_HiddenPath;
+
+    //
+    public GameObject player;
+
+    public bool shouldRespawn;
+
+    public GameObject spawnPoint;
+
 
     public void FireCoin() //When Fire coin is collected
     {
         F_SecretDoor.SetActive(false);
         F_SecretDoor2.SetActive(false);
         F_Fire.SetActive(false);
+        F_HiddenPath.SetActive(true);
         CheckCoins();
     }
 
@@ -42,4 +52,19 @@ public class Manager : MonoBehaviour
     {
 
     }
+
+    public void KillPlayer(bool isDead)
+    {
+        shouldRespawn = isDead;
+    }
+
+    private void Update()
+    {
+        if (shouldRespawn)
+        {
+            Debug.Log("DIE");
+            player.transform.position = spawnPoint.transform.position;
+        }
+    }
+
 }
